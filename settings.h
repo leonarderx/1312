a08839744ce841beb71daf17eaa826fd6b1cf02c
leonarderx <92988489+leonarderx@users.noreply.github.com>
2022-12-13 18:28:40 +0300

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+const float WINDOW_WIDTH = 800;
+const float WINDOW_HEIGHT = 600;
+const std::string WINDOW_TITLE = "SFML Lesson2";
+const float FPS = 60.f;
+const float batWidth = 20.f;
+const float batHeight = 80.f;
+const float batOffset = 50.f;
+const sf::Vector2f batSize(20.f, 80.f);
+const sf::Color leftBatColor{ 158,20,37 };
+const sf::Color rightBatColor{ 20,158,49 };
+const float batSpeed = 5.f;
+const sf::Vector2f leftBatStartPos{ batOffset, (WINDOW_HEIGHT - batHeight) / 2 };
+const sf::Vector2f rightBatStartPos{ batOffset, (WINDOW_HEIGHT - batHeight) / 2 };
+
+
+const float ballRadius = 25.f;
+const sf::Color ballColor{ sf::Color::Yellow };
